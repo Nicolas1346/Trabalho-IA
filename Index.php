86a26ee9224fb99a46f261452e3939105562b862
Nicolas Borges De Freitas <nicolasborges1346@gmail.com>
2026-09-22 06:41:44 -0300

    Change language attribute and update HTML structure

--- Index.php
+++ Index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +7,27 @@
     <title>reeze imoveis</title>
 </head>
 <body>
-    <button class="button">Clique aqui</button>    
+
+<nav class="navbar">
+
+    <div class="site-name">
+        <small>REZZE IMÓVEIS<small>
+    </div>
+
+    <div class="nav-links">
+        <a href="#">Início</a>
+        <a href="#">Imóveis</a>
+        <a href="#">Sobre nós</a>
+        <a href="#">Contato</a>
+    </div>
+
+</nav>
+
+    <h1>Seu próximo capítulo <br></br>
+    começa aqui.</h1>
+
+    <button type="button">Clique aqui</button>
+
+    
 </body>
 </html>
